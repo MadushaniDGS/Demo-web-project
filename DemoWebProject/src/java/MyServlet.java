bd23e72e75a2e64c2f9330106abe9614b5c0c3ee
Supuni Madushani <119606198+MadushaniDGS@users.noreply.github.com>
2024-02-18 15:12:44 +0530

    Add files via upload

---
+++ DemoWebProject/src/java/MyServlet.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Madushani
+ */
+@WebServlet(urlPatterns = {"/MyServlet"})
+public class MyServlet extends HttpServlet {
+
+     @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //action perform in doget method
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        
+        //accept value of html text
+        String name = request.getParameter("name");
+        
+        //display
+        out.print("You entered : "+name);
+    }
+}
